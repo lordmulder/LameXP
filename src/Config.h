de86e298ac2f77b264e571c9e978317243d6cb1e
lordmulder <mulder2@gmx.de>
2011-02-18 01:05:21 +0100

    Bump version.

--- src/Config.h
+++ src/Config.h
@@ -25,8 +25,8 @@
 #define VER_LAMEXP_MAJOR				4
 #define VER_LAMEXP_MINOR_HI				0
 #define VER_LAMEXP_MINOR_LO				0
-#define VER_LAMEXP_BUILD				319
-#define VER_LAMEXP_SUFFIX				RC-2
+#define VER_LAMEXP_BUILD				320
+#define VER_LAMEXP_SUFFIX				RC-3
 
 /*
  * Tools versions
