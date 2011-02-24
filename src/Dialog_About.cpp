bdee102e351468e6efea4b73f93b8d9704ef87e7
lordmulder <mulder2@gmx.de>
2011-02-24 13:41:21 +0100

    Updated LAME binary to v3.99 Alpha-12 (2011-02-13), compiled with ICL 12.0.2.

--- src/Dialog_About.cpp
+++ src/Dialog_About.cpp
@@ -262,14 +262,14 @@ void AboutDialog::showMoreAbout(void)
 	moreAboutText += makeToolText
 	(
 		tr("LAME &minus; OpenSource mp3 Encoder"),
-		"lame.exe", "v?.?? a??",
+		"lame.exe", "v?.??, Alpha-??",
 		tr("Released under the terms of the GNU Lesser General Public License."),
 		"http://lame.sourceforge.net/"
 	);
 	moreAboutText += makeToolText
 	(
 		tr("OggEnc &minus; Ogg Vorbis Encoder"),
-		"oggenc2_i386.exe", "v?.??",
+		"oggenc2_i386.exe", "v?.??, aoTuV Beta-?.??",
 		tr("Completely open and patent-free audio encoding technology."),
 		"http://www.vorbis.com/"
 	);
