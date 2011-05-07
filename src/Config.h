9df1febaecd956ab7d040c86e8b0f2c5929fe2b6
lordmulder <mulder2@gmx.de>
2011-05-07 15:50:21 +0200

    Updated FAQ document.

--- src/Config.h
+++ src/Config.h
@@ -30,7 +30,7 @@
 #define VER_LAMEXP_MINOR_LO					2
 #define VER_LAMEXP_TYPE						Alpha
 #define VER_LAMEXP_PATCH					13
-#define VER_LAMEXP_BUILD					492
+#define VER_LAMEXP_BUILD					493
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tools versions
