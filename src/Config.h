b63ac4afd5d99f5c9863f13655ea7f8df50fc4bc
lordmulder <mulder2@gmx.de>
2011-05-23 21:48:12 +0200

    Updated 'avs2wav' tool one more time.

--- src/Config.h
+++ src/Config.h
@@ -30,7 +30,7 @@
 #define VER_LAMEXP_MINOR_LO					2
 #define VER_LAMEXP_TYPE						Beta
 #define VER_LAMEXP_PATCH					3
-#define VER_LAMEXP_BUILD					548
+#define VER_LAMEXP_BUILD					549
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tools versions
