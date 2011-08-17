85b2a5326f261ad3a8d67659d47dbd4fd5d08357
lordmulder <mulder2@gmx.de>
2011-08-17 22:54:15 +0200

    Updated MediaInfo binary to v0.7.48 (2011-08-17), compiled with MSVC 10.0

--- src/Config.h
+++ src/Config.h
@@ -30,7 +30,7 @@
 #define VER_LAMEXP_MINOR_LO					3
 #define VER_LAMEXP_TYPE						Alpha
 #define VER_LAMEXP_PATCH					12
-#define VER_LAMEXP_BUILD					646
+#define VER_LAMEXP_BUILD					648
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tools versions
