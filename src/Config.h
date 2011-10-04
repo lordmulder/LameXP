f753f3ad418d1579a95f8589c592f66305b035c8
lordmulder <mulder2@gmx.de>
2011-10-04 23:17:34 +0200

    Updated translator's guide + FAQ document.

--- src/Config.h
+++ src/Config.h
@@ -30,7 +30,7 @@
 #define VER_LAMEXP_MINOR_LO					3
 #define VER_LAMEXP_TYPE						Beta
 #define VER_LAMEXP_PATCH					3
-#define VER_LAMEXP_BUILD					713
+#define VER_LAMEXP_BUILD					714
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tools versions
