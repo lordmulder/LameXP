22c25d334ea92a780bb0e24bc0e2010e1cb24d47
lordmulder <mulder2@gmx.de>
2011-10-09 22:52:54 +0200

    Updated Web Updater and Elevator binaries. Now with newer StdUtils plug-in.

--- src/Config.h
+++ src/Config.h
@@ -30,7 +30,7 @@
 #define VER_LAMEXP_MINOR_LO					3
 #define VER_LAMEXP_TYPE						Beta
 #define VER_LAMEXP_PATCH					3
-#define VER_LAMEXP_BUILD					725
+#define VER_LAMEXP_BUILD					726
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tools versions
