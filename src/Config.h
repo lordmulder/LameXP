10daa60c5322f465db57bf72964695fdecdbbcc5
lordmulder <mulder2@gmx.de>
2011-10-31 11:55:42 +0100

    Version v4.03 is released!

--- src/Config.h
+++ src/Config.h
@@ -28,9 +28,9 @@
 #define VER_LAMEXP_MAJOR					4
 #define VER_LAMEXP_MINOR_HI					0
 #define VER_LAMEXP_MINOR_LO					3
-#define VER_LAMEXP_TYPE						RC
-#define VER_LAMEXP_PATCH					3
-#define VER_LAMEXP_BUILD					763
+#define VER_LAMEXP_TYPE						Final
+#define VER_LAMEXP_PATCH					1
+#define VER_LAMEXP_BUILD					764
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tools versions
