11acbb60a0b1188d3368de747945a58dda0e5616
lordmulder <mulder2@gmx.de>
2011-11-22 23:51:50 +0100

    Bump minimum required QAAC version to 1.05.

--- src/Config.h
+++ src/Config.h
@@ -38,7 +38,7 @@
 
 #define VER_LAMEXP_TOOL_NEROAAC				1540
 #define VER_LAMEXP_TOOL_FHGAACENC			20110822
-#define VER_LAMEXP_TOOL_QAAC				104
+#define VER_LAMEXP_TOOL_QAAC				105
 #define VER_LAMEXP_TOOL_COREAUDIO			7710
 
 ///////////////////////////////////////////////////////////////////////////////
