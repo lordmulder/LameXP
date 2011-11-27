c1b88a7d07981ca993029a68da3135c87f87f6e2
lordmulder <mulder2@gmx.de>
2011-11-27 02:06:03 +0100

    Slightly tweaked formula to selected the number of instances based on CPU count. Maximum value is now 16.

--- src/Config.h
+++ src/Config.h
@@ -30,7 +30,7 @@
 #define VER_LAMEXP_MINOR_LO					4
 #define VER_LAMEXP_TYPE						Alpha
 #define VER_LAMEXP_PATCH					4
-#define VER_LAMEXP_BUILD					792
+#define VER_LAMEXP_BUILD					793
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tool versions (minimum expected versions!)
