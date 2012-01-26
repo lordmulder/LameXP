871866606d5fea991813427140cc900e74f771f3
lordmulder <mulder2@gmx.de>
2012-01-26 21:32:53 +0100

    Updated Musepack decoder binary to revision 475 (2011-08-10), compiled with ICL 12.1.6 and MSVC 10.0.

--- src/Config.h
+++ src/Config.h
@@ -30,7 +30,7 @@
 #define VER_LAMEXP_MINOR_LO					4
 #define VER_LAMEXP_TYPE						Alpha
 #define VER_LAMEXP_PATCH					17
-#define VER_LAMEXP_BUILD					884
+#define VER_LAMEXP_BUILD					885
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tool versions (minimum expected versions!)
