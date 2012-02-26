2c7f740fb9fe036814d2937babffc6f2788ddbb5
lordmulder <mulder2@gmx.de>
2012-02-26 15:05:18 +0100

    Updated Changelog.

--- src/Config.h
+++ src/Config.h
@@ -29,8 +29,8 @@
 #define VER_LAMEXP_MINOR_HI					0
 #define VER_LAMEXP_MINOR_LO					4
 #define VER_LAMEXP_TYPE						Beta
-#define VER_LAMEXP_PATCH					2
-#define VER_LAMEXP_BUILD					893
+#define VER_LAMEXP_PATCH					3
+#define VER_LAMEXP_BUILD					894
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tool versions (minimum expected versions!)
