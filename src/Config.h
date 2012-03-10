84450ed6b394ae256945eea59d55b2e5a4a80735
lordmulder <mulder2@gmx.de>
2012-03-10 03:51:22 +0100

    Updated GnuPG binaries to v1.4.12, compiled with GCC 4.6.1.

--- src/Config.h
+++ src/Config.h
@@ -30,7 +30,7 @@
 #define VER_LAMEXP_MINOR_LO					4
 #define VER_LAMEXP_TYPE						Beta
 #define VER_LAMEXP_PATCH					5
-#define VER_LAMEXP_BUILD					905
+#define VER_LAMEXP_BUILD					906
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tool versions (minimum expected versions!)
