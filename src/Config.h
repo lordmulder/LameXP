c2a41d783f8da1608148b1bea56b8c65269d1816
lordmulder <mulder2@gmx.de>
2012-04-29 18:00:06 +0200

    Development open for version 4.05 now.

--- src/Config.h
+++ src/Config.h
@@ -27,10 +27,10 @@
 
 #define VER_LAMEXP_MAJOR					4
 #define VER_LAMEXP_MINOR_HI					0
-#define VER_LAMEXP_MINOR_LO					4
-#define VER_LAMEXP_TYPE						Final
+#define VER_LAMEXP_MINOR_LO					5
+#define VER_LAMEXP_TYPE						Alpha
 #define VER_LAMEXP_PATCH					1
-#define VER_LAMEXP_BUILD					989
+#define VER_LAMEXP_BUILD					990
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tool versions (minimum expected versions!)
