a49c58a96aed82589c372513ed92d824eb448a70
lordmulder <mulder2@gmx.de>
2012-07-12 00:32:09 +0200

    Updated mpg123 decoder binary to v1.14.3 (2012-07-01), compiled with GCC 4.6.1.

--- src/Config.h
+++ src/Config.h
@@ -29,8 +29,8 @@
 #define VER_LAMEXP_MINOR_HI					0
 #define VER_LAMEXP_MINOR_LO					5
 #define VER_LAMEXP_TYPE						Alpha
-#define VER_LAMEXP_PATCH					6
-#define VER_LAMEXP_BUILD					1048
+#define VER_LAMEXP_PATCH					7
+#define VER_LAMEXP_BUILD					1050
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tool versions (minimum expected versions!)
