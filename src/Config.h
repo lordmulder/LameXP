f026dce18e692fa212a4a041b9b2a0edd54de386
lordmulder <mulder2@gmx.de>
2012-08-13 17:42:28 +0200

    Updated Visual Studio 2008 project file.

--- src/Config.h
+++ src/Config.h
@@ -30,7 +30,7 @@
 #define VER_LAMEXP_MINOR_LO					5
 #define VER_LAMEXP_TYPE						Beta
 #define VER_LAMEXP_PATCH					2
-#define VER_LAMEXP_BUILD					1080
+#define VER_LAMEXP_BUILD					1081
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tool versions (minimum expected versions!)
