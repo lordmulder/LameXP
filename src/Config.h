9aeb6b6b64e65a14322139ac2b7869e6de979894
LoRd_MuldeR <mulder2@gmx.de>
2012-10-27 01:38:20 +0200

    Updated FAAD2 binary in order to fix a bug in the progress printing code, which caused a buffer overrun with very long file names.

--- src/Config.h
+++ src/Config.h
@@ -30,7 +30,7 @@
 #define VER_LAMEXP_MINOR_LO					6
 #define VER_LAMEXP_TYPE						RC
 #define VER_LAMEXP_PATCH					2
-#define VER_LAMEXP_BUILD					1158
+#define VER_LAMEXP_BUILD					1159
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tool versions (minimum expected versions!)
