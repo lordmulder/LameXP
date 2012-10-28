23f0b98205e633592fb3b6e6a4526548a54c156e
LoRd_MuldeR <mulder2@gmx.de>
2012-10-28 18:41:16 +0100

    Updated MediaInfo binaries to v0.7.61+ (2012-10-28), compiled with ICL 12.1.7 and MSVC 10.0.

--- etc/Translation/LameXP_SV.ts
+++ etc/Translation/LameXP_SV.ts
@@ -3236,105 +3236,105 @@
         <translation>Stäng</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="271"/>
-        <location filename="../../src/Dialog_Update.cpp" line="548"/>
+        <location filename="../../src/Dialog_Update.cpp" line="273"/>
+        <location filename="../../src/Dialog_Update.cpp" line="550"/>
         <source>Build</source>
         <translation>Build</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="272"/>
+        <location filename="../../src/Dialog_Update.cpp" line="274"/>
         <source>Unknown</source>
         <translation>Okänd</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="380"/>
+        <location filename="../../src/Dialog_Update.cpp" line="382"/>
         <source>Testing your internet connection, please wait...</source>
         <translation>Kontrollerar iInternetanslutningen, vänta...</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="400"/>
-        <location filename="../../src/Dialog_Update.cpp" line="992"/>
+        <location filename="../../src/Dialog_Update.cpp" line="402"/>
+        <location filename="../../src/Dialog_Update.cpp" line="994"/>
         <source>It appears that the computer currently is offline!</source>
         <translation>Datorn verkar inte vara ansluten till Internet!</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="403"/>
-        <location filename="../../src/Dialog_Update.cpp" line="995"/>
+        <location filename="../../src/Dialog_Update.cpp" line="405"/>
+        <location filename="../../src/Dialog_Update.cpp" line="997"/>
         <source>Please make sure your computer is connected to the internet and try again.</source>
         <translation>Tillse att datorn är ansluten till Internet och försök igen.</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="461"/>
+        <location filename="../../src/Dialog_Update.cpp" line="463"/>
         <source>Network connectivity test has failed!</source>
         <translation>Anslutningskontrollen misslyckades!</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="464"/>
+        <location filename="../../src/Dialog_Update.cpp" line="466"/>
         <source>Please make sure your internet connection is working properly and try again.</source>
         <translation>Tillse att Internetuppkopplingen fungerar korrekt och försök igen.</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="478"/>
+        <location filename="../../src/Dialog_Update.cpp" line="480"/>
         <source>Checking for new updates online, please wait...</source>
         <translation>Söker nya uppdateringar, vänta...</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="533"/>
+        <location filename="../../src/Dialog_Update.cpp" line="535"/>
         <source>Failed to fetch update information from server!</source>
         <translation>Kunde inte hämta uppdateringsinformation från servern!</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="538"/>
+        <location filename="../../src/Dialog_Update.cpp" line="540"/>
         <source>Sorry, the update server might be busy at this time. Plase try again later.</source>
         <translation>Uppdateringsservern kan vara upptagen. Försök senare.</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="550"/>
+        <location filename="../../src/Dialog_Update.cpp" line="552"/>
         <source>More information available at:</source>
         <translation>Mer information hittas på:</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="556"/>
+        <location filename="../../src/Dialog_Update.cpp" line="558"/>
         <source>A new version of LameXP is available!</source>
         <translation>En ny version av LameXP finns tillgänglig!</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="558"/>
+        <location filename="../../src/Dialog_Update.cpp" line="560"/>
         <source>We highly recommend all users to install this update as soon as possible.</source>
         <translation>Vi rekommenderar alla användare att installera denna uppdatering så snart som möjligt.</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="567"/>
+        <location filename="../../src/Dialog_Update.cpp" line="569"/>
         <source>No new updates available at this time.</source>
         <translation>Det finns inga nya uppdateringar.</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="569"/>
+        <location filename="../../src/Dialog_Update.cpp" line="571"/>
         <source>Your version of LameXP is still up-to-date. Please check for updates regularly!</source>
         <translation>Din version av LameXP är uppdaterad. Kontrollera regelbundet!</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="578"/>
+        <location filename="../../src/Dialog_Update.cpp" line="580"/>
         <source>Your version appears to be newer than the latest release.</source>
         <translation>Din version verkar vara nyare än senaste &apos;Release&apos;.</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="580"/>
+        <location filename="../../src/Dialog_Update.cpp" line="582"/>
         <source>This usually indicates your are currently using a pre-release version of LameXP.</source>
         <translation>Det innebär normalt att du använder en beta-version av LameXP.</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="873"/>
+        <location filename="../../src/Dialog_Update.cpp" line="875"/>
         <source>Update is being downloaded, please be patient...</source>
         <translation>Uppdatering laddas ner, vänta...</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="918"/>
+        <location filename="../../src/Dialog_Update.cpp" line="920"/>
         <source>Update ready to install. Applicaion will quit...</source>
         <translation>Uppdatering klar att installeras. Programmet kommer att avslutas...</translation>
     </message>
     <message>
-        <location filename="../../src/Dialog_Update.cpp" line="926"/>
+        <location filename="../../src/Dialog_Update.cpp" line="928"/>
         <source>Update failed. Please try again or download manually!</source>
         <translation>Uppdateringen misslyckades. Försök igen, eller ladda ner manuellt!</translation>
     </message>
--- src/Config.h
+++ src/Config.h
@@ -30,7 +30,7 @@
 #define VER_LAMEXP_MINOR_LO					6
 #define VER_LAMEXP_TYPE						RC
 #define VER_LAMEXP_PATCH					3
-#define VER_LAMEXP_BUILD					1162
+#define VER_LAMEXP_BUILD					1163
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tool versions (minimum expected versions!)
