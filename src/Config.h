837f0588e7c3bd65c9387e54b42781c2fbf707b0
LoRd_MuldeR <mulder2@gmx.de>
2012-10-28 22:25:35 +0100

    Bum version.

--- src/Config.h
+++ src/Config.h
@@ -29,8 +29,8 @@
 #define VER_LAMEXP_MINOR_HI					0
 #define VER_LAMEXP_MINOR_LO					6
 #define VER_LAMEXP_TYPE						RC
-#define VER_LAMEXP_PATCH					3
-#define VER_LAMEXP_BUILD					1163
+#define VER_LAMEXP_PATCH					4
+#define VER_LAMEXP_BUILD					1164
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tool versions (minimum expected versions!)
