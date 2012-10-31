7770e021dae36a1b54b100921328dd977e9ff227
LoRd_MuldeR <mulder2@gmx.de>
2012-10-31 22:26:51 +0100

    Bump version.

--- src/Config.h
+++ src/Config.h
@@ -29,8 +29,8 @@
 #define VER_LAMEXP_MINOR_HI					0
 #define VER_LAMEXP_MINOR_LO					6
 #define VER_LAMEXP_TYPE						RC
-#define VER_LAMEXP_PATCH					4
-#define VER_LAMEXP_BUILD					1166
+#define VER_LAMEXP_PATCH					5
+#define VER_LAMEXP_BUILD					1168
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tool versions (minimum expected versions!)
