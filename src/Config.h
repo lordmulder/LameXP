15697af84d1e9fdb644b8887291ccae4403658cf
lordmulder <mulder2@gmx.de>
2012-11-17 21:54:11 +0100

    Make VS2010 configurations use the Windows SDK v7.1. Needs to set the LXP_WINSDK_ROOT environment variable.

--- src/Config.h
+++ src/Config.h
@@ -30,7 +30,7 @@
 #define VER_LAMEXP_MINOR_LO					7
 #define VER_LAMEXP_TYPE						Alpha
 #define VER_LAMEXP_PATCH					3
-#define VER_LAMEXP_BUILD					1192
+#define VER_LAMEXP_BUILD					1194
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tool versions (minimum expected versions!)
