1fe90b4feb24f42902a794e02c5a59f070130577
LoRd_MuldeR <mulder2@gmx.de>
2013-02-02 02:01:09 +0100

    Updated Web Updater and Elevator binaries. Now has massive code clean-up.

--- src/Config.h
+++ src/Config.h
@@ -30,7 +30,7 @@
 #define VER_LAMEXP_MINOR_LO					7
 #define VER_LAMEXP_TYPE						Beta
 #define VER_LAMEXP_PATCH					2
-#define VER_LAMEXP_BUILD					1235
+#define VER_LAMEXP_BUILD					1236
 
 ///////////////////////////////////////////////////////////////////////////////
 // Tool versions (minimum expected versions!)
