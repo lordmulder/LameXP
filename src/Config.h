555f8709a6ddd29db73f083c79a86b9597be5f19
LoRd_MuldeR <mulder2@gmx.de>
2014-04-09 17:09:52 +0200

    Updated MediaInfo binaries to v0.7.68 (2014-04-08), compiled with ICL 14.1 and MSVC 12.0.

--- src/Config.h
+++ src/Config.h
@@ -34,8 +34,8 @@
 #define VER_LAMEXP_MINOR_HI					1
 #define VER_LAMEXP_MINOR_LO					0
 #define VER_LAMEXP_TYPE						Alpha
-#define VER_LAMEXP_PATCH					2
-#define VER_LAMEXP_BUILD					1532
+#define VER_LAMEXP_PATCH					3
+#define VER_LAMEXP_BUILD					1534
 #define VER_LAMEXP_CONFG					1528
 
 ///////////////////////////////////////////////////////////////////////////////
