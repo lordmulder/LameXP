e44d9937bc2b74f42942925f445bc30e1e9bf485
LoRd_MuldeR <mulder2@gmx.de>
2014-05-29 01:00:07 +0200

    Updated VS2010 project file to include the compatibility manifest.

--- src/Config.h
+++ src/Config.h
@@ -35,7 +35,7 @@
 #define VER_LAMEXP_MINOR_LO					0
 #define VER_LAMEXP_TYPE						RC
 #define VER_LAMEXP_PATCH					1
-#define VER_LAMEXP_BUILD					1551
+#define VER_LAMEXP_BUILD					1553
 #define VER_LAMEXP_CONFG					1528
 
 ///////////////////////////////////////////////////////////////////////////////
