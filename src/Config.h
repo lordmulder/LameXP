833fa84f2bc74d0ad4efc6f9f710bca66cffa6dc
LoRd_MuldeR <mulder2@gmx.de>
2015-04-11 12:49:17 +0200

    Removed some old cruft.

--- src/Config.h
+++ src/Config.h
@@ -35,7 +35,7 @@
 #define VER_LAMEXP_MINOR_LO					2
 #define VER_LAMEXP_TYPE						Alpha
 #define VER_LAMEXP_PATCH					1
-#define VER_LAMEXP_BUILD					1703
+#define VER_LAMEXP_BUILD					1704
 #define VER_LAMEXP_CONFG					1700
 
 ///////////////////////////////////////////////////////////////////////////////
