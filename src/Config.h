0eb708e9b22d7fb11a95fe9154ad53dd8f3bc9c6
LoRd_MuldeR <mulder2@gmx.de>
2015-06-21 16:33:57 +0200

    Fixed compilation with latest MUtilities version.

--- src/Config.h
+++ src/Config.h
@@ -35,7 +35,7 @@
 #define VER_LAMEXP_MINOR_LO					2
 #define VER_LAMEXP_TYPE						Alpha
 #define VER_LAMEXP_PATCH					7
-#define VER_LAMEXP_BUILD					1758
+#define VER_LAMEXP_BUILD					1759
 #define VER_LAMEXP_CONFG					1700
 
 ///////////////////////////////////////////////////////////////////////////////
