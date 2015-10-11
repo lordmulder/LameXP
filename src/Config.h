c33a853aaef38e6973aec4eed2a3bf1dafda8d6e
LoRd_MuldeR <mulder2@gmx.de>
2015-10-11 17:32:42 +0200

    Silenced some DEBUG outputs.

--- src/Config.h
+++ src/Config.h
@@ -35,7 +35,7 @@
 #define VER_LAMEXP_MINOR_LO					2
 #define VER_LAMEXP_TYPE						RC
 #define VER_LAMEXP_PATCH					2
-#define VER_LAMEXP_BUILD					1806
+#define VER_LAMEXP_BUILD					1807
 #define VER_LAMEXP_CONFG					1700
 
 ///////////////////////////////////////////////////////////////////////////////
