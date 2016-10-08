00a9b586c61de31ebcdb55b6b8de12c0c8cac1f2
LoRd_MuldeR <mulder2@gmx.de>
2016-10-08 13:35:15 +0200

    Updated SFX EXE-header (7zSD.sfx).

--- src/Config.h
+++ src/Config.h
@@ -34,8 +34,8 @@
 #define VER_LAMEXP_MINOR_HI					1
 #define VER_LAMEXP_MINOR_LO					4
 #define VER_LAMEXP_TYPE						Beta
-#define VER_LAMEXP_PATCH					7
-#define VER_LAMEXP_BUILD					1909
+#define VER_LAMEXP_PATCH					8
+#define VER_LAMEXP_BUILD					1910
 #define VER_LAMEXP_CONFG					1818
 
 ///////////////////////////////////////////////////////////////////////////////
