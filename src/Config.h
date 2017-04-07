9a85c1d1dd53a69eea5ecb7ce5b3b9f9d2e9caeb
LoRd_MuldeR <mulder2@gmx.de>
2017-04-07 20:49:12 +0200

    Adapted for the latest MUtils library changes.

--- src/Config.h
+++ src/Config.h
@@ -35,7 +35,7 @@
 #define VER_LAMEXP_MINOR_LO					5
 #define VER_LAMEXP_TYPE						Beta
 #define VER_LAMEXP_PATCH					2
-#define VER_LAMEXP_BUILD					1968
+#define VER_LAMEXP_BUILD					1969
 #define VER_LAMEXP_CONFG					1934
 
 ///////////////////////////////////////////////////////////////////////////////
