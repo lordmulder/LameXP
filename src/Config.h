fa92f086e467a4156e89a3c1a1b01b096735167a
LoRd_MuldeR <mulder2@gmx.de>
2017-05-06 22:25:29 +0200

    Updated Opus encoder/decoder libraries v1.2-alpha2 (2017-05-06). Re-built to fix a problem with "i686" build on the processor without SSE/SSE2 support.

--- src/Config.h
+++ src/Config.h
@@ -35,7 +35,7 @@
 #define VER_LAMEXP_MINOR_LO					5
 #define VER_LAMEXP_TYPE						Beta
 #define VER_LAMEXP_PATCH					7
-#define VER_LAMEXP_BUILD					1994
+#define VER_LAMEXP_BUILD					1996
 #define VER_LAMEXP_CONFG					1934
 
 ///////////////////////////////////////////////////////////////////////////////
