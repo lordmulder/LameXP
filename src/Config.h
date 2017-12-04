7e5786ba3073d4ad95539b82222202de9c7154a3
LoRd_MuldeR <mulder2@gmx.de>
2017-12-04 22:33:18 +0100

    Updated Opus encoder/decoder libraries v1.2.1-35 (2017-11-16) and Opus-Tools to v0.1.10-9 (2017-05-25).

--- src/Config.h
+++ src/Config.h
@@ -35,7 +35,7 @@
 #define VER_LAMEXP_MINOR_LO					6
 #define VER_LAMEXP_TYPE						Alpha
 #define VER_LAMEXP_PATCH					10
-#define VER_LAMEXP_BUILD					2058
+#define VER_LAMEXP_BUILD					2059
 #define VER_LAMEXP_CONFG					2002
 
 ///////////////////////////////////////////////////////////////////////////////
