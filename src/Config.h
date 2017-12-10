4d7b7a766ab61ec2e69703a21aba9980b2be16cd
LoRd_MuldeR <mulder2@gmx.de>
2017-12-10 16:40:48 +0100

    Updated mpg123 decoder to v1.25.8 (2017-12-02), compiled with GCC 7.2.0.

--- src/Config.h
+++ src/Config.h
@@ -35,7 +35,7 @@
 #define VER_LAMEXP_MINOR_LO					6
 #define VER_LAMEXP_TYPE						Beta
 #define VER_LAMEXP_PATCH					1
-#define VER_LAMEXP_BUILD					2065
+#define VER_LAMEXP_BUILD					2066
 #define VER_LAMEXP_CONFG					2002
 
 ///////////////////////////////////////////////////////////////////////////////
