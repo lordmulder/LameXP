e0aeb53be80314420f52f8deba3ddc7f7bf92ccd
LoRd_MuldeR <mulder2@gmx.de>
2018-02-06 19:58:22 +0100

    Updated MediaInfo binaries to v17.12 (2017-12-21), compiled with ICL 18.0 and MSVC 14.1.

--- src/Config.h
+++ src/Config.h
@@ -35,7 +35,7 @@
 #define VER_LAMEXP_MINOR_LO					6
 #define VER_LAMEXP_TYPE						Beta
 #define VER_LAMEXP_PATCH					5
-#define VER_LAMEXP_BUILD					2089
+#define VER_LAMEXP_BUILD					2090
 #define VER_LAMEXP_CONFG					2002
 
 ///////////////////////////////////////////////////////////////////////////////
