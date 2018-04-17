cced655a449445c4e9813bb2cd42bd2ffc926df0
LoRd_MuldeR <mulder2@gmx.de>
2018-04-17 20:24:57 +0200

    Re-compiled GnuPG binary v1.4.22 (2017-07-19) in order to fix WinXP compatibility, compiled with GCC 7.3.0.

--- src/Config.h
+++ src/Config.h
@@ -34,8 +34,8 @@
 #define VER_LAMEXP_MINOR_HI					1
 #define VER_LAMEXP_MINOR_LO					6
 #define VER_LAMEXP_TYPE						RC
-#define VER_LAMEXP_PATCH					3
-#define VER_LAMEXP_BUILD					2120
+#define VER_LAMEXP_PATCH					4
+#define VER_LAMEXP_BUILD					2124
 #define VER_LAMEXP_CONFG					2002
 
 ///////////////////////////////////////////////////////////////////////////////
