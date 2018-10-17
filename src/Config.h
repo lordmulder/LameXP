392a1af0ccefe7b2360f7b986c94633d5ca413aa
LoRd_MuldeR <mulder2@gmx.de>
2018-10-17 21:49:31 +0200

    Bump version.

--- src/Config.h
+++ src/Config.h
@@ -35,7 +35,7 @@
 #define VER_LAMEXP_MINOR_LO					7
 #define VER_LAMEXP_TYPE						RC
 #define VER_LAMEXP_PATCH					2
-#define VER_LAMEXP_BUILD					2166
+#define VER_LAMEXP_BUILD					2168
 #define VER_LAMEXP_CONFG					2134
 
 ///////////////////////////////////////////////////////////////////////////////
