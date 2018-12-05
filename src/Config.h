a0c390ff5649053b0120e2e1873d142f9ceb636f
LoRd_MuldeR <mulder2@gmx.de>
2018-12-05 21:47:16 +0100

    Added x64 binaries of mpg123 decoder v1.25.10 (2018-03-05), compiled with GCC 8.2.1.

--- src/Config.h
+++ src/Config.h
@@ -34,8 +34,8 @@
 #define VER_LAMEXP_MINOR_HI					1
 #define VER_LAMEXP_MINOR_LO					8
 #define VER_LAMEXP_TYPE						Alpha
-#define VER_LAMEXP_PATCH					1
-#define VER_LAMEXP_BUILD					2190
+#define VER_LAMEXP_PATCH					2
+#define VER_LAMEXP_BUILD					2192
 #define VER_LAMEXP_CONFG					2188
 
 ///////////////////////////////////////////////////////////////////////////////
