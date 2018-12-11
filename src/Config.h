a0cdc8d44508849adbda20ea25f5b36b18f01a1a
LoRd_MuldeR <mulder2@gmx.de>
2018-12-11 17:02:03 +0100

    Updated Monkey's Audio binaries to v4.41 (2018-12-05), compiled with ICL 19.0 and MSVC 14.16.

--- src/Config.h
+++ src/Config.h
@@ -35,7 +35,7 @@
 #define VER_LAMEXP_MINOR_LO					8
 #define VER_LAMEXP_TYPE						Alpha
 #define VER_LAMEXP_PATCH					3
-#define VER_LAMEXP_BUILD					2198
+#define VER_LAMEXP_BUILD					2199
 #define VER_LAMEXP_CONFG					2188
 
 ///////////////////////////////////////////////////////////////////////////////
