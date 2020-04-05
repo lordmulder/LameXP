862392110e77de44f0bdb6945276b9e3d87b1832
LoRd_MuldeR <mulder2@gmx.de>
2020-04-05 23:10:46 +0200

    Updated MediaInfo binaries to v20.03 (2020-04-03), compiled with ICL 19.1 and MSVC 15.9.

--- src/Config.h
+++ src/Config.h
@@ -34,8 +34,8 @@
 #define VER_LAMEXP_MINOR_HI					1
 #define VER_LAMEXP_MINOR_LO					9
 #define VER_LAMEXP_TYPE						Alpha
-#define VER_LAMEXP_PATCH					1
-#define VER_LAMEXP_BUILD					2244
+#define VER_LAMEXP_PATCH					2
+#define VER_LAMEXP_BUILD					2247
 #define VER_LAMEXP_CONFG					2188
 
 ///////////////////////////////////////////////////////////////////////////////
