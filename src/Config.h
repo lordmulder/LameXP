3aa48ce4205132af7d0d44e70fdd91e243a02df6
LoRd_MuldeR <mulder2@gmx.de>
2020-11-06 22:32:02 +0100

    Bump version.

--- src/Config.h
+++ src/Config.h
@@ -34,8 +34,8 @@
 #define VER_LAMEXP_MINOR_HI					1
 #define VER_LAMEXP_MINOR_LO					9
 #define VER_LAMEXP_TYPE						Beta
-#define VER_LAMEXP_PATCH					3
-#define VER_LAMEXP_BUILD					2280
+#define VER_LAMEXP_PATCH					4
+#define VER_LAMEXP_BUILD					2282
 #define VER_LAMEXP_CONFG					2188
 
 ///////////////////////////////////////////////////////////////////////////////
