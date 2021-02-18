lordmulder/LameXP

commit de228da9c51a3a1fc330442b5f6d9059e28b9e26

Small code clean-up.

src/Dialog_About.cpp
@@ -250,19 +250,19 @@ AboutDialog::~AboutDialog(void)
 
 int AboutDialog::exec()
 {
-	if(m_settings->soundsEnabled())
+	if(m_firstStart)
 	{
-		if(m_firstStart)
+		if (m_settings->soundsEnabled())
 		{
-			if(!MUtils::Sound::play_sound_file("imageres.dll", 5080, true))
+			if (!MUtils::Sound::play_sound_file("imageres.dll", 5080, true))
 			{
 				MUtils::Sound::play_system_sound("SystemStart", true);
 			}
 		}
-		else
-		{
-			PLAY_SOUND_OPTIONAL("ghost", true);
-		}
+	}
+	else
+	{
+		PLAY_SOUND_OPTIONAL("ghost", true);
 	}
 	
 	switch(QDialog::exec())
@@ -331,10 +331,7 @@ void AboutDialog::tabChanged(int index, const bool silent)
 	}
 
 	//Play tick sound
-	if(m_settings->soundsEnabled() && (!silent))
-	{
-		PLAY_SOUND_OPTIONAL("tick", true);
-	}
+	PLAY_SOUND_OPTIONAL("tick", true);
 
 	//Scroll to the top
 	if(QWidget *tab = ui->tabWidget->widget(index))

commit 952f0a91d4f1419bb8ecb294aa5a2e553dc5ee36

In AboutDialog::tabChanged(), do not play the tick sound, if 'silent' flag is set. Fixes regression introduced in de228da9c51a3a1fc330442b5f6d9059e28b9e26.

src/Config.h
@@ -35,7 +35,7 @@
 #define VER_LAMEXP_MINOR_LO					9
 #define VER_LAMEXP_TYPE						RC
 #define VER_LAMEXP_PATCH					3
-#define VER_LAMEXP_BUILD					2294
+#define VER_LAMEXP_BUILD					2295
 #define VER_LAMEXP_CONFG					2188
 
 ///////////////////////////////////////////////////////////////////////////////

src/Dialog_About.cpp
@@ -331,8 +331,11 @@ void AboutDialog::tabChanged(int index, const bool silent)
 		qApp->restoreOverrideCursor();
 	}
 
-	//Play tick sound
-	PLAY_SOUND_OPTIONAL("tick", true);
+	//Play tick sound, unless silent
+	if (!silent)
+	{
+		PLAY_SOUND_OPTIONAL("tick", true);
+	}
 
 	//Scroll to the top
 	if(QWidget *tab = ui->tabWidget->widget(index))