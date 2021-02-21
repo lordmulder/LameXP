55754ee5298208e5f827ffe9680041b387fbac6c
LoRd_MuldeR <mulder2@gmx.de>
2021-02-21 13:54:15 +0100

    Small code simplification.

--- src/Config.h
+++ src/Config.h
@@ -35,7 +35,7 @@
 #define VER_LAMEXP_MINOR_LO					9
 #define VER_LAMEXP_TYPE						RC
 #define VER_LAMEXP_PATCH					3
-#define VER_LAMEXP_BUILD					2295
+#define VER_LAMEXP_BUILD					2297
 #define VER_LAMEXP_CONFG					2188
 
 ///////////////////////////////////////////////////////////////////////////////
