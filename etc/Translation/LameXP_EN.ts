0da27bde9f24df2a79c176adca56ac557ef911da
Felipe <felipefplzx@gmail.com>
2021-05-01 11:26:49 -0300

    Create LameXP_PT_BR.ts
    Added a brazilian portuguese translation for lamexp.

--- etc/Translation/LameXP_EN.ts
+++ etc/Translation/LameXP_EN.ts
@@ -7,24 +7,24 @@
         <location filename="../../src/Dialog_CueImport.cpp" line="442"/>
         <source>Process was aborted by the user after %n track(s)!</source>
         <translation>
-            <numerusform>Process was aborted by the user after %n track!</numerusform>
-            <numerusform>Process was aborted by the user after %n tracks!</numerusform>
+            <numerusform>O processo foi abortado pelo usuário após %n faixa!</numerusform>
+            <numerusform>O processo foi abortado pelo usuário após %n faixas!</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../../src/Dialog_CueImport.cpp" line="450"/>
         <source>Imported %n track(s) from the Cue Sheet.</source>
         <translation>
-            <numerusform>Imported %n track from the Cue Sheet.</numerusform>
-            <numerusform>Imported %n tracks from the Cue Sheet.</numerusform>
+            <numerusform>Importou %n faixa da Folha do Cue.</numerusform>
+            <numerusform>Importou %n faixas da Folha do Cue.</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../../src/Dialog_CueImport.cpp" line="450"/>
         <source>Skipped %n track(s).</source>
         <translation>
-            <numerusform>Skipped %n track.</numerusform>
-            <numerusform>Skipped %n tracks.</numerusform>
+            <numerusform>Ignorou %n faixa.</numerusform>
+            <numerusform>Ignorou %n faixas.</numerusform>
         </translation>
     </message>
 </context>
@@ -34,40 +34,40 @@
         <location filename="../../src/Dialog_MainWindow.cpp" line="859"/>
         <source>%n file(s) have been rejected, because read access was not granted!</source>
         <translation>
-            <numerusform>%n file have been rejected, because read access was not granted!</numerusform>
-            <numerusform>%n files have been rejected, because read access was not granted!</numerusform>
+            <numerusform>%n arquivo foi rejeitado porque o acesso a leitura não foi garantido!</numerusform>
+            <numerusform>%n arquivo foram rejeitados porque o acesso a leitura não foi garantido!!</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../../src/Dialog_MainWindow.cpp" line="863"/>
         <source>%n file(s) have been rejected, because they are dummy CDDA files!</source>
         <translation>
-            <numerusform>%n file have been rejected, because they are dummy CDDA files!</numerusform>
-            <numerusform>%n files have been rejected, because they are dummy CDDA files!</numerusform>
+            <numerusform>%n arquivo foi rejeitado porque ele é um arquivo de imitação do CDDA!</numerusform>
+            <numerusform>%n arquivos foram rejeitados porque eles são arquivos de imitação do CDDA!</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../../src/Dialog_MainWindow.cpp" line="867"/>
         <source>%n file(s) have been rejected, because they appear to be Cue Sheet images!</source>
         <translation>
-            <numerusform>%n file have been rejected, because they appear to be Cue Sheet images!</numerusform>
-            <numerusform>%n files have been rejected, because they appear to be Cue Sheet images!</numerusform>
+            <numerusform>%n arquivo foi rejeitado porque ele aparenta ser imagem da Folha do Cue!</numerusform>
+            <numerusform>%n arquivos foram rejeitados porque eles aparentam ser imagens da Folha do Cue!</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../../src/Dialog_MainWindow.cpp" line="871"/>
         <source>%n file(s) have been rejected, because the file format could not be recognized!</source>
         <translation>
-            <numerusform>%n file have been rejected, because the file format could not be recognized!</numerusform>
-            <numerusform>%n files have been rejected, because the file format could not be recognized!</numerusform>
+            <numerusform>%n arquivo foi rejeitado porque o formato do arquivo não pôde ser reconhecido!</numerusform>
+            <numerusform>%n arquivos foram rejeitados porque o formato dos arquivos não pôde ser reconhecido!</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../../src/Dialog_MainWindow.cpp" line="4251"/>
         <source>%n Instance(s)</source>
         <translation>
-            <numerusform>%n Instance</numerusform>
-            <numerusform>%n Instances</numerusform>
+            <numerusform>%n Instância</numerusform>
+            <numerusform>%n Instâncias</numerusform>
         </translation>
     </message>
 </context>
@@ -77,56 +77,56 @@
         <location filename="../../src/Dialog_Processing.cpp" line="666"/>
         <source>Encoding: %n file(s) of %1 completed so far, please wait...</source>
         <translation>
-            <numerusform>Encoding: %n file of %1 completed so far, please wait...</numerusform>
-            <numerusform>Encoding: %n files of %1 completed so far, please wait...</numerusform>
+            <numerusform>Codificando: %n arquivo de %1 completado até agora, por favor espere...</numerusform>
+            <numerusform>Codificando: %n arquivos de %1 completados até agora, por favor espere...</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../../src/Dialog_Processing.cpp" line="698"/>
         <source>Process was aborted by the user after %n file(s)!</source>
         <translation>
-            <numerusform>Process was aborted by the user after %n file!</numerusform>
-            <numerusform>Process was aborted by the user after %n files!</numerusform>
+            <numerusform>O processo foi abortado pelo usuário após %n arquivo!</numerusform>
+            <numerusform>O processo foi abortado pelo usuário após %n arquivos!</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../../src/Dialog_Processing.cpp" line="719"/>
         <source>Error: %1 of %n file(s) failed (%2). Double-click failed items for detailed information!</source>
         <translation>
-            <numerusform>Error: %1 of %n file failed (%2). Double-click failed items for detailed information!</numerusform>
-            <numerusform>Error: %1 of %n files failed (%2). Double-click failed items for detailed information!</numerusform>
+            <numerusform>Erro: %1 de %n arquivo falhou (%2). O clique duplo falhou pra obter informação detalhada sobre o item!</numerusform>
+            <numerusform>Erro: %1 de %n arquivos falharam (%2). O clique duplo falhou pra obter informação detalhada sobre os itens!</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../../src/Dialog_Processing.cpp" line="719"/>
         <source>%n file(s) skipped</source>
         <translation>
-            <numerusform>%n file skipped</numerusform>
-            <numerusform>%n files skipped</numerusform>
+            <numerusform>%n arquivo ignorado</numerusform>
+            <numerusform>%n arquivos ignorados</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../../src/Dialog_Processing.cpp" line="723"/>
         <source>Error: %1 of %n file(s) failed. Double-click failed items for detailed information!</source>
         <translation>
-            <numerusform>Error: %1 of %n file failed. Double-click failed items for detailed information!</numerusform>
-            <numerusform>Error: %1 of %n files failed. Double-click failed items for detailed information!</numerusform>
+            <numerusform>Error: %1 de %n arquivo falhou. O clique duplo falhou pra obter informação detalhada sobre o item!</numerusform>
+            <numerusform>Error: %1 de %n arquivos falharam. O clique duplo falhou pra obter informação detalhada sobre os itens!</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../../src/Dialog_Processing.cpp" line="737"/>
         <source>All files completed successfully. Skipped %n file(s).</source>
         <translation>
-            <numerusform>All files completed successfully. Skipped %n file.</numerusform>
-            <numerusform>All files completed successfully. Skipped %n files.</numerusform>
+            <numerusform>Todos os arquivos completados com sucesso. Ignorou %n arquivo.</numerusform>
+            <numerusform>Todos os arquivos completados com sucesso. Ignorou %n arquivos.</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../../src/Dialog_Processing.cpp" line="1205"/>
         <source>%n hour(s)</source>
         <translation>
-            <numerusform>%n hour</numerusform>
-            <numerusform>%n hours</numerusform>
+            <numerusform>%n hora</numerusform>
+            <numerusform>%n horas</numerusform>
         </translation>
     </message>
     <message numerus="yes">
@@ -134,8 +134,8 @@
         <location filename="../../src/Dialog_Processing.cpp" line="1210"/>
         <source>%n minute(s)</source>
         <translation>
-            <numerusform>%n minute</numerusform>
-            <numerusform>%n minutes</numerusform>
+            <numerusform>%n minuto</numerusform>
+            <numerusform>%n minutos</numerusform>
         </translation>
     </message>
     <message numerus="yes">
@@ -143,16 +143,16 @@
         <location filename="../../src/Dialog_Processing.cpp" line="1215"/>
         <source>%n second(s)</source>
         <translation>
-            <numerusform>%n second</numerusform>
-            <numerusform>%n seconds</numerusform>
+            <numerusform>%n segundo</numerusform>
+            <numerusform>%n segundos</numerusform>
         </translation>
     </message>
     <message numerus="yes">
         <location filename="../../src/Dialog_Processing.cpp" line="1216"/>
         <source>%n millisecond(s)</source>
         <translation>
-            <numerusform>%n millisecond</numerusform>
-            <numerusform>%n milliseconds</numerusform>
+            <numerusform>%n mili-segundo</numerusform>
+            <numerusform>%n mili-segundos</numerusform>
         </translation>
     </message>
 </context>
