65611691db06705ecfaa3bb313957dab4924f2ec
LoRd_MuldeR <mulder2@gmx.de>
2021-07-26 22:50:38 +0200

    Bump version.

--- src/Config.h
+++ src/Config.h
@@ -34,8 +34,8 @@
 #define VER_LAMEXP_MINOR_HI					1
 #define VER_LAMEXP_MINOR_LO					9
 #define VER_LAMEXP_TYPE						RC
-#define VER_LAMEXP_PATCH					10
-#define VER_LAMEXP_BUILD					2316
+#define VER_LAMEXP_PATCH					11
+#define VER_LAMEXP_BUILD					2317
 #define VER_LAMEXP_CONFG					2188
 
 ///////////////////////////////////////////////////////////////////////////////
