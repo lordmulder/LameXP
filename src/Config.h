52275c20fdf23321888e403c73021f1e614bd095
LoRd_MuldeR <mulder2@gmx.de>
2022-10-09 18:49:29 +0200

    Updated MediaInfo binaries to v22.09 (2022-10-04), compiled with ICL 2022.2 and MSVC 15.9.

--- src/Config.h
+++ src/Config.h
@@ -35,7 +35,7 @@
 #define VER_LAMEXP_MINOR_LO					0
 #define VER_LAMEXP_TYPE						Beta
 #define VER_LAMEXP_PATCH					4
-#define VER_LAMEXP_BUILD					2349
+#define VER_LAMEXP_BUILD					2350
 #define VER_LAMEXP_CONFG					2188
 
 ///////////////////////////////////////////////////////////////////////////////
