161b88b90300a628c39684225b8996ea7805d81b
LoRd_MuldeR <mulder2@gmx.de>
2022-11-10 20:36:41 +0100

    Fixed checksum for Mozilla CA certificate list.

--- src/Config.h
+++ src/Config.h
@@ -34,8 +34,8 @@
 #define VER_LAMEXP_MINOR_HI					2
 #define VER_LAMEXP_MINOR_LO					0
 #define VER_LAMEXP_TYPE						Beta
-#define VER_LAMEXP_PATCH					5
-#define VER_LAMEXP_BUILD					2351
+#define VER_LAMEXP_PATCH					6
+#define VER_LAMEXP_BUILD					2352
 #define VER_LAMEXP_CONFG					2188
 
 ///////////////////////////////////////////////////////////////////////////////
