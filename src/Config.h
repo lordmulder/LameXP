a79a14ac5c211e5516d9af1218934288ae586db6
LoRd_MuldeR <mulder2@gmx.de>
2023-04-08 12:25:38 +0200

    Updated VS2019 solution to use CompatLib in order to restore WinXP compatibility + drop old VS2015 solution.

--- src/Config.h
+++ src/Config.h
@@ -35,7 +35,7 @@
 #define VER_LAMEXP_MINOR_LO					0
 #define VER_LAMEXP_TYPE						Final
 #define VER_LAMEXP_PATCH					1
-#define VER_LAMEXP_BUILD					2356
+#define VER_LAMEXP_BUILD					2358
 #define VER_LAMEXP_CONFG					2188
 
 ///////////////////////////////////////////////////////////////////////////////
