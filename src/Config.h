3afa3be1cae4d3518e00afb41aa84acd328216eb
LoRd_MuldeR <mulder2@gmx.de>
2023-12-12 23:21:34 +0100

    Updated LAME encoder to v3.101 Beta-3 (2023-11-21), compiled with ICL 2023.2 and MSVC 16.11.

--- src/Config.h
+++ src/Config.h
@@ -35,7 +35,7 @@
 #define VER_LAMEXP_MINOR_LO					1
 #define VER_LAMEXP_TYPE						Beta
 #define VER_LAMEXP_PATCH					4
-#define VER_LAMEXP_BUILD					2365
+#define VER_LAMEXP_BUILD					2366
 #define VER_LAMEXP_CONFG					2188
 
 ///////////////////////////////////////////////////////////////////////////////
